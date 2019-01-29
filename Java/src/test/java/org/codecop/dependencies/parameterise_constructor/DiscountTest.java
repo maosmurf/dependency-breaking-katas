7a4c0e95168b7cf017a0589487fbde77ed8ecf9a
Adam Zielinski <adam.zielinski@kurier.at>
2019-01-29 18:35:25 +0100

    testCrazySalesDay

--- Java/src/test/java/org/codecop/dependencies/parameterise_constructor/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/parameterise_constructor/DiscountTest.java
@@ -1,14 +1,30 @@
 package org.codecop.dependencies.parameterise_constructor;
 
-import java.math.BigDecimal;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DiscountTest {
 
     @Test
-    public void test1() {
+    public void testCrazySalesDay() {
+
+        MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
+        when(marketingCampaign.isCrazySalesDay()).thenReturn(true);
+        Discount discount = new Discount(marketingCampaign);
+
+        Money net = new Money(100);
+        Money total = discount.discountFor(net);
+
+        assertEquals(new Money(new BigDecimal("85")), total);
+    }
+
+    @Test
+    public void testOverOneThousand() {
         Discount discount = new Discount();
 
         Money net = new Money(1002);
@@ -16,5 +32,6 @@ public class DiscountTest {
 
         assertEquals(new Money(new BigDecimal("901.8")), total);
     }
+
 
 }
